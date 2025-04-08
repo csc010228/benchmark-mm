be74229b61b7dfc7e897ae97b6e17510dbf4a9fb
csc010228 <csc010228@163.com>
2025-04-08 10:25:25 +0800

    train-time isolated

--- userbenchmark/train-time/run.py
+++ userbenchmark/train-time/run.py
@@ -1,12 +1,10 @@
 import argparse
 import itertools
 import json
-import time
 from datetime import datetime
 from typing import List
 from pathlib import Path
 import shlex
-import textwrap
 
 from ..utils import add_path, dump_output, get_output_dir, get_output_json, REPO_PATH
 
@@ -19,17 +17,12 @@ with add_path(REPO_PATH):
     )
     from torchbenchmark.util.experiment.instantiator import (
         list_models,
-        load_model,
-        BenchmarkModel,
         TorchBenchModelConfig,
     )
     from torchbenchmark.util.experiment.metrics import (
         get_model_test_metrics,
         TorchBenchModelMetrics,
         NANOSECONDS_PER_MILLISECONDS
-    )
-    from torchbenchmark.util.extra_args import (
-        TEST_STAGE
     )
 
 WORKER_TIMEOUT = 3600  # seconds
@@ -39,6 +32,10 @@ BM_NAME = "train-time"
 DEFAULT_ROUNDS = 3
 DEFAULT_BATCH_SIZE = 1
 DEFAULT_ITERATIONS = 15
+DEFAULT_ALLOCATOR = ""
+DEFAULT_ALLOC_FUNC = "alloc"
+DEFAULT_FREE_FUNC = "free"
+DEFAULT_CACHE_INFO_FUNC = "cache_info"
 
 WARMUP_ROUNDS = 0
 
@@ -106,6 +103,27 @@ def parse_args(args: List[str]):
             "Supports quoted values and special characters. "
             "Example: --envs \"CUDA_VISIBLE_DEVICES=0 PYTORCH_CUDA_ALLOC_CONF=backend:native,max_split_size_mb:128\"."
         ),
+    )
+    parser.add_argument(
+        "-a",
+        "--allocator",
+        default=DEFAULT_ALLOCATOR,
+        help="The .so file of user defined allocator.",
+    )
+    parser.add_argument(
+        "--alloc-func",
+        default=DEFAULT_ALLOC_FUNC,
+        help="Alloc function name of user defined allocator. Default is \"alloc\".",
+    )
+    parser.add_argument(
+        "--free-func",
+        default=DEFAULT_FREE_FUNC,
+        help="Free function name of user defined allocator. Default is \"free\".",
+    )
+    parser.add_argument(
+        "--cache_info-func",
+        default=DEFAULT_CACHE_INFO_FUNC,
+        help="Cache info function name of user defined allocator. Default is \"cache_info\".",
     )
     parser.add_argument("-d", "--device", default="cuda", help="Specify the device.")
     parser.add_argument(
@@ -204,54 +222,89 @@ def generate_filter(args: argparse.Namespace):
 
     return cfg_filter
 
-run_times = []
-forward_times = []
-backward_times = []
-optimizer_times = []
 
-class RunTimer:
+def load_model_isolated_with_allocator_and_timer(
+    config: TorchBenchModelConfig, allocator: str, alloc_func: str, free_func: str, cache_info_func: str, timeout: float = WORKER_TIMEOUT
+) -> ModelTask:
+    """Load and return the model in a subprocess. Optionally, save its stdout and stderr to the specified directory."""
+    task = ModelTask(
+        config.name,
+        timeout=timeout,
+        extra_env=config.extra_env,
+        save_output_dir=config.output_dir,
+    )
+    task.worker.run("import time")
+    task.worker.run("from torchbenchmark.util.extra_args import (TEST_STAGE)")
+    task.worker.run("""class RunTimer:
     def __enter__(self):
         self.t0 = time.time_ns()
         return self
         
     def __exit__(self, exc_type, exc_value, traceback):
         self.t1 = time.time_ns()
-        run_times.append(self.t1 - self.t0)
-
-class ForwardTimer:
+        run_times.append(self.t1 - self.t0)""")
+    task.worker.run("""class ForwardTimer:
     def __enter__(self):
         self.t0 = time.time_ns()
         return self
         
     def __exit__(self, exc_type, exc_value, traceback):
         self.t1 = time.time_ns()
-        forward_times.append(self.t1 - self.t0)
-
-class BackwardTimer:
+        forward_times.append(self.t1 - self.t0)""")
+    task.worker.run("""class BackwardTimer:
     def __enter__(self):
         self.t0 = time.time_ns()
         return self
         
     def __exit__(self, exc_type, exc_value, traceback):
         self.t1 = time.time_ns()
-        backward_times.append(self.t1 - self.t0)
-
-class OptimizerTimer:
+        backward_times.append(self.t1 - self.t0)""")
+    task.worker.run("""class OptimizerTimer:
     def __enter__(self):
         self.t0 = time.time_ns()
         return self
         
     def __exit__(self, exc_type, exc_value, traceback):
         self.t1 = time.time_ns()
-        optimizer_times.append(self.t1 - self.t0)
-
-def load_model_with_timer(config: TorchBenchModelConfig) -> BenchmarkModel:
-    model = load_model(config)
-    model.add_context(RunTimer, TEST_STAGE.ALL)
-    model.add_context(ForwardTimer, TEST_STAGE.FORWARD)
-    model.add_context(BackwardTimer, TEST_STAGE.BACKWARD)
-    model.add_context(OptimizerTimer, TEST_STAGE.OPTIMIZER)
-    return model
+        optimizer_times.append(self.t1 - self.t0)""")
+    task.worker.store("run_times", [])
+    task.worker.store("forward_times", [])
+    task.worker.store("backward_times", [])
+    task.worker.store("optimizer_times", [])
+    if allocator != DEFAULT_ALLOCATOR:
+        task.worker.run(
+            f"""
+            customized_alloc = torch.cuda.memory.CUDAPluggableAllocator('{allocator}', '{alloc_func}', '{free_func}')
+            customized_alloc.set_cache_info_fn('{cache_info_func}')
+            torch.cuda.memory.change_current_allocator(customized_alloc)
+        """
+        )
+    if not task.model_details.exists:
+        raise ValueError(
+            f"Failed to import model task: {config.name}. Please run the model manually to make sure it succeeds, or report a bug."
+        )
+    task.make_model_instance(
+        test=config.test,
+        device=config.device,
+        batch_size=config.batch_size,
+        extra_args=config.extra_args,
+    )
+    task.worker.run("model.add_context(RunTimer, TEST_STAGE.ALL)")
+    task.worker.run("model.add_context(ForwardTimer, TEST_STAGE.FORWARD)")
+    task.worker.run("model.add_context(BackwardTimer, TEST_STAGE.BACKWARD)")
+    task.worker.run("model.add_context(OptimizerTimer, TEST_STAGE.OPTIMIZER)")
+    task_batch_size = task.get_model_attribute(BS_FIELD_NAME)
+    # check batch size if not measuring accuracy
+    if (
+        config.batch_size
+        and (not config.batch_size == task_batch_size)
+        and not task.get_model_attribute("accuracy")
+    ):
+        raise ValueError(
+            f"User specify batch size {config.batch_size},"
+            + f"but model {task.name} runs with batch size {task_batch_size}. Please report a bug."
+        )
+    return task
 
 def run(args: List[str]):
     args = parse_args(args)
@@ -269,11 +322,15 @@ def run(args: List[str]):
         for cfg in filter(cfg_filter, cfgs):
             print(f"[Round {_round}/{args.rounds}] Running {cfg}")
             try:
-                task = load_model_with_timer(cfg)
+                task = load_model_isolated_with_allocator_and_timer(cfg, args.allocator, args.alloc_func, args.free_func, args.cache_info_func)
                 # get the model test metrics
                 metrics: TorchBenchModelMetrics = get_model_test_metrics(
                     task, metrics=["latencies"], num_iter=args.iterations
                 )
+                run_times = task.worker.load("run_times")
+                forward_times = task.worker.load("forward_times")
+                backward_times = task.worker.load("backward_times")
+                optimizer_times = task.worker.load("optimizer_times")
                 raw_metrics = {
                     "run_times": [x / NANOSECONDS_PER_MILLISECONDS for x in run_times],
                     "forward_times": [x / NANOSECONDS_PER_MILLISECONDS for x in forward_times],
@@ -312,10 +369,6 @@ def run(args: List[str]):
             finally:
                 # Remove task reference to trigger deletion in gc
                 task = None
-                run_times.clear()
-                forward_times.clear()
-                backward_times.clear()
-                optimizer_times.clear()
         full_results.append(single_round_result)
     # reduce full results to metrics
     # log detailed results in the .userbenchmark/model-stableness/logs/ directory
